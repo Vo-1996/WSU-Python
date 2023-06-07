45c4e28ef22a06eb8d6b467804dd32f9d2edb99b
michaelvo <michaelvo61096@gmail.com>
2023-06-06 22:38:06 -0500

    hi

--- Multiplication_Tables/Multiplication_Tables.py
+++ Multiplication_Tables/Multiplication_Tables.py
@@ -6,15 +6,15 @@ while investigated_number <= -1:
     print ("Please enter a number between 0 through 10")
     investigated_number = int(input("investigated number: "))
 
-lower_range_number = int(input("low range number: "))
-while lower_range_number <= -1:
-    print("Please select a higher number")
-    lower_range_number = int(input("low range number: "))
+low_range_number = int(input("low range number: "))
+while low_range_number <= -1:
+    print("Please select a high number")
+    low_range_number = int(input("low range number: "))
 
-higher_range_number = int(input("high range number: "))
-while higher_range_number < lower_range_number:
-    print("select a number equal to or higher than your low range number")
-    higher_range_number = int(input("high range number: "))
+high_range_number = int(input("high range number: "))
+while high_range_number < low_range_number:
+    print("select a number equal to or high than your low range number")
+    high_range_number = int(input("high range number: "))
 
-for i in range (lower_range_number, higher_range_number + 1 ):
+for i in range (low_range_number, high_range_number + 1 ):
     print(investigated_number, "x", i, "=", investigated_number*i)
